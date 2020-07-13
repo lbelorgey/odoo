a75c4b41699879a5005e6c9c9f9ee595927f25a4
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-07-13 09:18:00 +0200

    [FIX] Ir Rule: _domain_force_get

--- odoo/addons/base/models/ir_rule.py
+++ odoo/addons/base/models/ir_rule.py
@@ -132,7 +132,6 @@ class IrRule(models.Model):
         self._cr.execute(query, (model_name, self._uid))
         return self.browse(row[0] for row in self._cr.fetchall())
 
-    @api.model
     @tools.conditional('xml' not in config['dev_mode'],
         tools.ormcache('self.env.uid', 'self.env.su',
                        'tuple(self._compute_domain_context_values())'),
@@ -140,18 +139,16 @@ class IrRule(models.Model):
     def _domain_force_get(self):
         # browse user and rules as SUPERUSER_ID to avoid access errors!
         eval_context = self._eval_context()
-        user_groups = self.env.user.groups_id
-        global_domains = []  # list of domains
-        group_domains = []  # list of domains
+        res = {}
         for rule in self.sudo():
-            # evaluate the domain for the current user
-            dom = safe_eval(rule.domain_force,
-                            eval_context) if rule.domain_force else []
-            dom = expression.normalize_domain(dom)
-            if not rule.groups:
-                global_domains.append(dom)
-            elif rule.groups & user_groups:
-                group_domains.append(dom)
+            res.setdefault(rule.id, [])
+            if rule.domain_force:
+                # evaluate the domain for the current user
+                dom = safe_eval(rule.domain_force,
+                                eval_context) if rule.domain_force else []
+                dom = expression.normalize_domain(dom)
+                res[rule.id] = dom
+        return res
    
     @api.model
     @tools.conditional(

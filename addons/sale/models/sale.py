176f09f62aa74affe180a11e838fc592bd4e94cb
Denis Roussel <denis.roussel@acsone.eu>
2022-04-06 15:15:46 +0200

    [IMP] sale: Restore the fix function for price unit

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -1715,6 +1715,7 @@ class SaleOrderLine(models.Model):
                 product_price_unit=self._get_display_price(product),
                 product_currency=self.order_id.currency_id
             )
+            vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(vals['price_unit'], product.taxes_id, self.tax_id, self.company_id)
         self.update(vals)
 
         title = False
@@ -1756,6 +1757,8 @@ class SaleOrderLine(models.Model):
                 product_price_unit=self._get_display_price(product),
                 product_currency=self.order_id.currency_id
             )
+            self.price_unit = self.env['account.tax']._fix_tax_included_price_company(
+                vals['price_unit'], product.taxes_id, self.tax_id, self.company_id)
 
     def name_get(self):
         result = []

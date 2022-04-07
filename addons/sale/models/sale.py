2fb472e24341d8eb2068e5579a91480feaaa7442
Denis Roussel <denis.roussel@acsone.eu>
2022-04-07 11:33:41 +0200

    [FIX] sale: Use correct value for price unit

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -1715,7 +1715,7 @@ class SaleOrderLine(models.Model):
                 product_price_unit=self._get_display_price(product),
                 product_currency=self.order_id.currency_id
             )
-            vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(vals['price_unit'], product.taxes_id, self.tax_id, self.company_id)
+            vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
         self.update(vals)
 
         title = False
@@ -1758,7 +1758,7 @@ class SaleOrderLine(models.Model):
                 product_currency=self.order_id.currency_id
             )
             self.price_unit = self.env['account.tax']._fix_tax_included_price_company(
-                vals['price_unit'], product.taxes_id, self.tax_id, self.company_id)
+                self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
 
     def name_get(self):
         result = []

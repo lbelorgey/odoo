1d67f4795b8571d04ea740180e9c9da8df02674c
Ivan Yelizariev <iel@odoo.com>
2022-06-21 08:49:12 +0000

    [FIX] web: fix space symbol in monetary field's title in list view
    Browsers don't render html entity in the "title" attribute and we cannot use
    them. Otherwise we may get values like "$&nbsp;100".
    
    Fix it by using special character and not html entity. It works both in html
    strings and attributues
    
    The problem was reported here:
    https://github.com/odoo/odoo/pull/90191#issuecomment-1156034355
    
    STEPS: open invoice and put pointer over monetary field in the list.
    
    Note, that to reproduce the problem, the widget should not be specifed
    explicitly. See
    https://github.com/odoo/odoo/blob/5b9fada39913cd456a3659faef1160f84ae19860/addons/web/static/src/js/views/list/list_renderer.js#L436-L439
    https://github.com/odoo/odoo/blob/5b9fada39913cd456a3659faef1160f84ae19860/addons/web/static/src/js/views/list/list_renderer.js#L452-L456
    
    closes odoo/odoo#96232
    
    X-original-commit: d2d9bf1b874b33083bdbcfb08e30616bf3cce920
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Ivan Elizaryev (iel) <iel@odoo.com>

--- addons/web/static/src/js/fields/field_utils.js
+++ addons/web/static/src/js/fields/field_utils.js
@@ -23,6 +23,8 @@ var utils = require('web.utils');
 
 var _t = core._t;
 
+const NBSP = "\u00a0";
+
 //------------------------------------------------------------------------------
 // Formatting
 //------------------------------------------------------------------------------
@@ -323,6 +325,13 @@ function formatX2Many(value) {
  *        the number of digits that should be used, instead of the default
  *        digits precision in the field. Note: if the currency defines a
  *        precision, the currency's one is used.
+ * @param {boolean} [options.forceString=false]
+ *        if true, returns a string with regular whitespace. Otherwise it uses
+ *        non-breaking whitespace unicode character. The option is presented for
+ *        historical reason and will be removed in master. Previous
+ *        implementation used html entity `&nbsp;`, which doesn't work in html
+ *        attributes. With new implementation we can always use the unicode
+ *        character and the option is not needed anymore.
  * @returns {string}
  */
 function formatMonetary(value, field, options) {
@@ -352,10 +361,11 @@ function formatMonetary(value, field, options) {
     if (!currency || options.noSymbol) {
         return formatted_value;
     }
+    const ws = options.forceString ? ' ' : NBSP;
     if (currency.position === "after") {
-        return formatted_value += '&nbsp;' + currency.symbol;
+        return formatted_value + ws + currency.symbol;
     } else {
-        return currency.symbol + '&nbsp;' + formatted_value;
+        return currency.symbol + ws + formatted_value;
     }
 }
 /**
@@ -552,6 +562,9 @@ function parseFloat(value) {
  */
 function parseMonetary(value, field, options) {
     var values = value.split('&nbsp;');
+    if (values.length === 1) {
+        values = value.split(NBSP);
+    }
     if (values.length === 1) {
         return parseFloat(value);
     }
